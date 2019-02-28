d3a742656042b5124bfd32f073f5cd1250f850a8
qh <870754310@qq.com>
2019-02-28 16:24:37 +0800

    提交

---
+++ eureka-config-client/src/main/java/com/qh/eurekaconfigclient/EurekaConfigClientApplication.java
@@ -0,0 +1,25 @@
+package com.qh.eurekaconfigclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class EurekaConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/index")
+    public String index() {
+        return foo;
+    }
+
+}
